451451a96a69c147896164c24380b6a987555244
Emmanuel Tetteh Totimeh <emmanueltotimeh2002@gmail.com>
2024-07-20 17:24:47 +0000

    printify added

---
+++ printify/printify/members/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.hello, name='hello'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about')
+]
---
+++ printify/printify/members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello World")
+
+def home(request):
+    return HttpResponse("Welcome to the homepage.")
+
+def about(request):
+    return render(request, 'index.html')
